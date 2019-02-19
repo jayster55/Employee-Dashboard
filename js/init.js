d991f255fdffc18d6e4272dd253d259ba5885b63
Jared Gee <jared.gee@guild1.com>
2019-02-19 09:54:32 -0700

    Commit 2: Added Calendars and Social Media Integrations

--- js/init.js
+++ js/init.js
@@ -8,36 +8,18 @@
 
   }); // end of document ready
 
-
-  document.addEventListener('DOMContentLoaded', function() {
-      var elems = document.querySelectorAll('.collapsible');
-      var instances = M.Collapsible.init(elems, options);
-    });
-
-
     $(document).ready(function(){
       $('.collapsible').collapsible();
     });
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.tap-target');
-        var instances = M.TapTarget.init(elems, options);
-      });
-
 
       $(document).ready(function(){
         $('.tap-target').tapTarget();
       });
 
-
-
-        // Or with jQuery
-
         $(document).ready(function(){
           $('.modal').modal();
           $('.modal').modal('open');
         });
-
 
         $(document).ready(function(){
             $('.carousel').carousel();
